f45a4186d3b938b33e77b4562f4ecf13f89f2bfb
Judy Mwai <juddiemukami@gmail.com>
2022-07-26 19:05:56 +0300

    creates states in the post component for author, title and content

--- developerblog/src/components/Posts.js
+++ developerblog/src/components/Posts.js
@@ -1,30 +1,34 @@
-// import React from "React"
+import React from "React"
 
 
-// function Posts(){
-//     return (
-//         // <form className='newPost'>
-//         //     <label>
-//         //         Title:
-//         //         <input type="text" title="title" value={title} onChange={(e)=>setTitle(e.target.value)}/>
+function Posts(){
+    const [title, setTitle]=useState('')
+    const [author, setAuthor]=useState('')
+    const [content, setContent]=useState('')
 
-//         //     </label>
-//         //     <label>
-//         //     Content:
-//         //         <input type="text" content="content" value={content} onChange={(e)=>setContent(e.target.value)}/>
+    return (
+        // <form className='newPost'>
+        //     <label>
+        //         Title:
+        //         <input type="text" title="title" value={title} onChange={(e)=>setTitle(e.target.value)}/>
 
-//         //     </label>
-//         //     <label>
-//         //     Author:
-//         //         <input type="text" author="author" value={author} onChange={(e)=>setAuthor(e.target.value)}/>
-//         //     </label>
-//         //     <button type="submit">Submit New Post</button>
+        //     </label>
+        //     <label>
+        //     Content:
+        //         <input type="text" content="content" value={content} onChange={(e)=>setContent(e.target.value)}/>
+
+        //     </label>
+        //     <label>
+        //     Author:
+        //         <input type="text" author="author" value={author} onChange={(e)=>setAuthor(e.target.value)}/>
+        //     </label>
+        //     <button type="submit">Submit New Post</button>
            
 
-//         // </form>
-//         <div>
-//             <h1>Posts</h1>
-//         </div>
-//     )
-// }
-// export default Posts
+        // </form>
+        <div>
+            <h1>Posts</h1>
+        </div>
+    )
+}
+export default Posts
